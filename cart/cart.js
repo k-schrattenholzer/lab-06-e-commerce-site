8b6b88a7c0b362d6a08bc97181e346036b9b675d
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-25 16:41:17 -0700

    added keep shopping button in the cart

--- cart/cart.js
+++ cart/cart.js
@@ -1,5 +1,5 @@
 import { getOrderTotal, renderTableRow } from './render-table.js';
-import { getCart, orderAlert, clearCart, returnUserHome } from './localstorage-utils.js';
+import { getCart, orderAlert, clearCart, returnUserHome, returnToProducts } from './localstorage-utils.js';
 import { seedPacks } from '../products/data.js';
 
 const cart = getCart();
@@ -8,6 +8,7 @@ const tableBody = document.querySelector('tbody');
 const total = getOrderTotal(seedPacks, cart);
 const totalTd = document.getElementById('total');
 const checkoutButton = document.getElementById('checkout-button');
+const backButton = document.getElementById('keep-shopping-button');
 
 for (let item of cart) {
 
@@ -22,4 +23,8 @@ checkoutButton.addEventListener('click', () =>{
     orderAlert();
     clearCart();
     returnUserHome();
+});
+
+backButton.addEventListener('click', () =>{
+    returnToProducts();
 });
